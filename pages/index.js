09333a25c3668976639ff5b2aa7fb5ff68bc85b2
Jeremy Yiu <28777843+Jeremyyiu@users.noreply.github.com>
2022-02-20 23:30:28 +1100

    added chakra UI provider setup

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-	return <div>Hello</div>
+	return (
+		<Container>
+			<Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+				Hello, I&apos;m a software engineer based in Sydney.
+			</Box>
+
+			<Box display={{md: 'flex'}}>
+			<Box flexGrow={1}>
+				<Heading as="h2" variant="page-title">
+					Jeremy Yiu
+				</Heading>
+				<p>Quality Assurance Engineer / Software Engineer</p>
+			</Box>
+			</Box>
+		</Container>
+	)
+
 }
 
 export default Page
